d93e4ae1a10883b90e4d0b01163f77ca156497ef
Padamshree <padamshree98@gmail.com>
2021-09-10 22:23:13 +0530

    Fixing gitingnore

---
+++ config/firebase.js
@@ -0,0 +1,42 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const firebase = require('firebase');
+const admin = require('firebase-admin');
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
+  };
+
+const serviceAccount = {
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY,
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: process.env.CLIENT_CERT_URL,
+};
+
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+module.exports = {
+    db,
+    auth,
+    admin,
+}
